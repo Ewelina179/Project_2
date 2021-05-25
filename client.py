8ce758b01affd2171e2c7633529ad95c2cb000f0
inka.179 <ewelinadalek@gmail.com>
2021-05-25 10:07:09 +0200

    sending from client command

--- client.py
+++ client.py
@@ -2,16 +2,38 @@ import socket
 import sys
 import json
 
-j = {"name": "Olek", "surname": "Nowak"} 
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+
+UPTIME_MESSAGE="UPTIME"
+INFO_MESSAGE = "INFO"
+HELP_MESSAGE = "HELP"
+DISCONNECT_MESSAGE = "STOP"
+DIC={"UPTIME_MESSAGE":"UPTIME", "INFO_MESSAGE":"INFO"}
+#SERVER = socket.gethostbyname(socket.gethostname())
+#ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 2738))
+
 
 
 data = json.dumps(j)
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('localhost', 2738))
+"""
     s.sendall(bytes(data,encoding="utf-8"))
     a = s.recv(1024)
     a = a.decode("utf-8")
     print ('Json: ', repr(a))
+"""
+def send_uptime(y):
+    for x in DIC:
+        if DIC[x]==y:
+            data = json.dumps(x)
+            client.sendall(bytes(data,encoding="utf-8"))
+            data=client.recv(1024)
+            data = data.decode("utf-8")
+            print('JSON:', repr(data))
+
     
-    
+send_uptime("UPTIME")
--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 import socket
 import time
+import threading
+import json
 
 ip_server='127.0.0.1'
 port=2738
+HEADER = 64
+FORMAT = 'utf-8'
 
+UPTIME_MESSAGE="UPTIME"
+INFO_MESSAGE = "INFO"
+HELP_MESSAGE = "HELP"
+DISCONNECT_MESSAGE = "STOP"
 
 #t = time.localtime()
 #print "time.asctime(t): %s " % time.asctime(t)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+    s.bind((ip_server, port))
     begin=time.time()
     print("Czas socketu bind: ", begin)
-    s.bind((ip_server, port))
     print("IP_serwera:", ip_server)
     s.listen(10)
     conn, addres=s.accept()
