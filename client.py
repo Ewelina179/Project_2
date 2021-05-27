66bc1a6c12f2d4267108d5707f45233c5018f4c6
inka.179 <ewelinadalek@gmail.com>
2021-05-27 10:53:49 +0200

    Changes in loop in server.py

--- client.py
+++ client.py
@@ -1,14 +1,8 @@
 import socket
-#import sys
 import json
 
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
 
 COMM=["UPTIME", "INFO", "HELP", "STOP"]
-#SERVER = socket.gethostbyname(socket.gethostname())
-#ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 2738))
@@ -21,7 +15,6 @@ def send_uptime(y):
         if COMM[x]==y:
             data["message"]=y
             data2 = json.dumps(data)
-            #print(data2)
             client.sendall(bytes(data2,encoding="utf-8"))
             data=client.recv(1024)
             data = data.decode("utf-8")
@@ -29,10 +22,9 @@ def send_uptime(y):
 
 
 print('You can choose command from list. Type {} to get information about commands.'.format(COMM[2]))
-
-#while True:
 z=input()
 send_uptime(z)
 if z=="STOP":
     client.close()
+
 
--- server.py
+++ server.py
@@ -1,18 +1,15 @@
 import socket
 import time
-import threading
 import json
 
 ip_server='127.0.0.1'
 port=2738
-HEADER = 64
-FORMAT = 'utf-8'
+
 
 MESSAGES=["UPTIME", "INFO", "HELP", "STOP"]
 MESSAGES_COMM={"UPTIME":"uptime", "INFO":"info", "HELP":"help", "STOP":"Your connection is closed"}
 MESSAGES_COMM["HELP"]=[["UPTIME","cos"], ["INFO", "cos"], ["STOP", "cos"]]
-#t = time.localtime()
-#print "time.asctime(t): %s " % time.asctime(t)
+
 
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
@@ -30,7 +27,6 @@ while True:
         end2=time.ctime(time.time())
         print ('Klient z adresu', addres)
         print("Czas połączenia z klientem: ", end2)
-    #while True:
         uptime_value={}
         uptime=end-begin
         MESSAGES_COMM["UPTIME"]=uptime
@@ -54,15 +50,4 @@ while True:
                 conn.send(bytes(data_to_server, encoding="utf-8"))
             elif MESSAGES[x] == "STOP":
                 connected=False
-    #server.sendall(bytes(data_to_server,encoding="utf-8"))
-#conn.close()
-#	client.send(time.ctime(time.time())) # wyslanie danych do klienta
-#wrzuć to w loop!
-"""
-if data["message"] == "INFO":
-                data3=json.dumps(info)
-                conn.send(bytes(data3, encoding="utf-8"))
-            if data["message"] == "HELP":
-                data4=json.dumps(help)
-                conn.send(bytes(data4, encoding="utf-8"))
-                """
+   
