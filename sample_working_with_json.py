ab8990887f6ceed656ed403c1e3e5c3a01a3b358
inka.179 <ewelinadalek@gmail.com>
2021-07-17 16:24:42 +0200

    added func read_all_msg

--- sample_working_with_json.py
+++ sample_working_with_json.py
@@ -1,9 +1,17 @@
 import json
+import os
+
+x=os.path.abspath('users.json')
+print(x)
 
 def find_user(username):
-    with open('users.json') as j:
+    x=os.path.abspath('users.json')
+    with open(x) as j:
         data=json.load(j)
     return any(username==subvalue["username"] for subvalue in data["users"])
+
+#wyżej filter(lambda)???
+# coś w ten, że user=next(filter(lambda x: x["username"]==username, data["users"]), None)
 
 def is_valid_password(username, password):
     with open('users.json') as j:
@@ -12,7 +20,21 @@ def is_valid_password(username, password):
             if username==x["username"]:
                 return password==x["password"]
 
+def read_all_msg(username):
+    with open('messages.json') as m:
+        data = json.load(m)
+        for x in data["users"]:
+            if username==x["username"]:
+                all_msg=x["messages from"]
+                return all_msg
+        #polskie znaki?
+
+def get_names_of_sender(username):
+    pass
+
+""""
 x=find_user("Ewelina")
 print(x)
 y=is_valid_password("Ewelina", "Ewelina12")
 print(y)
+"""
